6a19657f3ea99e5d872969dafedba008b96088d6
Rasmus Nielsen <rewn97@gmail.com>
2018-10-15 12:50:11 +0200

    Basic server/client communication established
    Also adds further player information, the client will write their name, then connect to the server and sends the name. The server will then create a new player object that can store the name

---
+++ src/Player.java
@@ -0,0 +1,17 @@
+public class Player {
+
+	private String playerName;
+	
+	public Player(String name) {
+		playerName = name;;
+	}
+	
+	public void setPLayerName(String s) {
+		playerName = s;
+	}
+	
+	public String getPlayerName() {
+		return playerName;
+	}
+	
+}
--- src/Server.java
+++ src/Server.java
@@ -1,52 +1,51 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Date;
-
-
+import java.util.ArrayList;
 
 public class Server {
+	
+	static ArrayList<Player> player = new ArrayList<Player>();
+	static int numberOfPlayers = 0;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		try {
-		     
-		      ServerSocket serverSocket = new ServerSocket(8000);
-		     System.out.println("Loan Server started at " + new Date() + '\n');
-
-		     
-		     while (true) {
-		    	
-		
-		
-		      Socket connectToClient = serverSocket.accept();
-		      
-		      new Thread (new RunClient(connectToClient)).start();
-
-		     
-		      System.out.println("Connected to a client " + " at " + new Date() + '\n');
-
-		     
-		    } 
-		      
-		  }
-		    catch(IOException e) {
+			ServerSocket serverSocket = new ServerSocket(8000);
+			System.out.println("Server started at " + new Date());
+			while (true) {
+				Socket connectToClient = serverSocket.accept();
+				new Thread (new RunClient(connectToClient)).start();
+				
+				
+			}
+		}catch(IOException e) {
 		      System.err.println(e);
-		    }
-		  }
+		      }
+		}
 }
 
 class RunClient implements Runnable {
 	
 	private Socket socket; 
+	Server server = new Server();
 	
 	public RunClient (Socket socket) {
 		this.socket = socket; 
-		
 	}
 	
 	public void run () {
-		
+		try {
+			DataInputStream in = new DataInputStream(socket.getInputStream());
+			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+			
+			String playerName = in.readLine();
+			Server.player.add(new Player(playerName));
+			System.out.println("Player " + Server.player.get(Server.numberOfPlayers).getPlayerName() + " has joined the server");
+		} catch (IOException e) {}
 		
 	}
 	
